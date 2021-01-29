ff8fb028ed013a026dbead3fef78186d81c9a5a2
Linux User <ket@min.my.domain>
2021-01-29 14:07:46 +0000

    first commit

---
+++ pages/api/products.js
@@ -0,0 +1,182 @@
+import rateLimit from '../../utils/ratelimit'
+import fetch from 'node-fetch'
+import Selly from '../../utils/selly'
+
+let r = null;
+
+const cat = {
+  "FOOD GIFTCARDS": [
+'Abuelos Gift Cards',
+'Arbys Gift Cards',
+'Back Yard Burgers Gift Cards',
+'Blaze Pizza Gift Cards',
+'Bob Evans Gift Cards',
+'Boston Market Gift Cards',
+'Brixx Wood Fired Pizza Gift Card',
+'Broken Yolk Cafe GC',
+'BurgerFi Gift Cards',
+'BWW Blazin\' Rewards',
+'Captain D\'s Gift Cards',
+'Chevy\'s Gift Cards',
+'City BBQ Gift Cards',
+'Coffee & Bagels Giftcards',
+'Country Kitchen Gift Cards',
+'Cracker Barrel Gift Card',
+'Craftworks Gift Cards ',
+'Crave American Kitchen Gift Cards',
+'Cream Nation Gift Card',
+'Dickeys BBQ Gift Card',
+'Donatos Gift Cards',
+'Dunkin\' Donuts Gift Cards',
+'Embers Restaurant & Trio Bistro GCS',
+'Points Firehouse',
+'First Watch Cafe Gift Card ',
+'Forever Yogurt GC',
+'Frisch\'s Big Boy Gift Cards',
+'Fuddruckers Gift Card',
+'Gilmore Collection Gift Card',
+'Golden Corral Gift Cards',
+'Graeters Gift Cards',
+'Hello Fresh OFF',
+'HelloFresh OFF discount code',
+'Hooter\'s Gift Card',
+'InsomniaCookies Rewards',
+'Jamba Juice Gift Cards',
+'Jamba, Auntie Annes, and more',
+'Jason\'s Deli Gift Card',
+'Jersey Mike\'s Free Giant Size Sub',
+'Jimmy John\'s',
+'Johnny Rockets Gift Cards',
+'Jollibee Gift Cards',
+'La Rosa Pizzeria Gift Cards',
+'Lazy Dog Restaurant Gift Cards',
+'Macaroni Grill Gift Cards',
+'Main Event Gift Card',
+'McAlister\'s Deli Gift Cards',
+'Melting Pot Gift Cards',
+'Mission BBQ Gift Cards',
+'Moe\'s Southwest Grill Gift Cards',
+'Norms Gift Cards',
+'Nothing Bundt Cakes GC',
+'Old Country Buffet Gift Cards',
+'Panera Bread Accounts',
+'Pei Wei Gift Cards',
+'Penn Station Gift Cards',
+'Piada Italian Gift Cards',
+'Pinkberry Gift Card',
+'Pizza Hut Code',
+'Potbelly Gift Cards',
+'Pressed Juicery GC Accounts',
+'Quiznos Gift Card',
+'Red Robins',
+'Robeks Gift Cards',
+'Rocky Mountain Chocolate Gift Card',
+'Roundtable Pizza Gift Cards',
+'Runza Gift Card',
+'Salsaritas Gift Cards',
+'Shane\'s Rib Shack Gift Card',
+'Shoney\'s Gift Cards',
+'Souplantation & ST Gift Cards',
+'Starbucks Gift Cards',
+'Subways Accounts',
+'Tacobell Gift Card',
+'Tcby Yogurt Gift Cards',
+'TGI Friday\'s Points Account',
+'Tijuana Flats Gift Card',
+'Tom Chee Gift Cards',
+'Uncle Julio\'s Gift Cards',
+'Village Inn Gift Cards',
+'Zoe\'s Kitchen Gift Cards',
+  ],
+ "CLOTHES GIFTCARDS & OFF CODES": [
+'Abercrombie Rewards Cash',
+'Adidas OFF Code',
+'Carter\'s/OshKosh Reward Accounts',
+'Dick\'s Sporting Goods Reward',
+'GAP Rewards',
+'Justice Rewards',
+'Kmart/Sears Rewards',
+'Roadrunner Sports Gift Cards',
+'Shoe Carnival Gift Cards',
+'UNTUCKit Gift Card',
+'Vera Bradley Gift Cards',
+ ],
+
+  "BEAUTY REWARDS & GIFTCARDS": [
+'Bath & Body Works Rewards Account',
+'Art of Shaving Gift Cards',
+'Nyx Beauty Gift Card',
+'Sally Beauty Supply Rewards Free Item',
+'The Body Shop Rewards',
+'Ulta Rewards',
+  ],
+
+  "STREAMING / ENTERTAINMENT": [
+'AMC Stubs',
+'AMC Theaters',
+'AT&T TV NOW Go Big & HBO',
+'Cobb / CinéBistro Theaters',
+'DirectTv',
+'Dish America\'s',
+'iPic Theaters Gift Cards',
+'NBA League Pass',
+'NBA TV',
+'NFL Sunday Ticket MAX',
+'Pandora',
+  ]
+}
+
+const limiter = rateLimit({
+  interval: 60 * 1000,
+  uniqueTokenPerInterval: 300
+})
+
+const getRessy = async () => {
+  if(r) return r;
+  const res = (await (await fetch('http://45.90.109.181/products.json')).json());
+  let categories = {OTHERS:{}};
+
+  for(const product of res) {
+    let added = 0;
+    let realname = product.title.replace(/(\d+[^a-z0-9](\s)?|\/|\$|^\s|:|\+|\'|’|(^\$\d\.\d\s))/gm, '')
+      .replace(/\s{1,}/gm, ' ')
+      .split(/\s(-|\d|\[|\$|\()/g)[0]
+      .split(' ')
+
+    realname = realname.slice(0, 2 + (realname[1] === '&')).join(' ');
+
+    for(const key in cat) {
+      const c = cat[key].find(_ => _.replace('\'', '').toLowerCase().includes(realname.toLowerCase()))
+      if(c) {
+        if(!categories[key]) categories[key] = {};
+        if(!categories[key][realname]) categories[key][realname] = [];
+        categories[key][realname].push(product);
+        added = 1;
+        break;
+      }
+    }
+    
+    if(!added) {
+      if(!categories['OTHERS'][realname]) categories['OTHERS'][realname] = [];
+      categories['OTHERS'][realname].push(product);
+    }
+  }
+
+  r = categories;
+  return r;
+}
+
+export default async (req,res) => {
+
+  if(await limiter.check(res, 40, 'CACHE_TOKEN'))
+    return res.status(429).json({error: 'Slow down cowboy.'})
+
+  try {
+    const data = await getRessy();
+    res.json({data});
+  }
+  catch(e) {
+    console.log(e);
+    res.json({error: 'Contact the owner as soon as possible'});
+  }
+}
---
+++ pages/index.js
@@ -0,0 +1,131 @@
+import Head from 'next/head'
+import PopUp from '../components/popup';
+import { useState, useRef, useEffect } from 'react';
+import AOS from 'aos'
+
+import styles from '../styles/Home.module.css'
+import "aos/dist/aos.css"
+
+
+
+export default function Home({ data }) {
+
+  const [query, setQuery] = useState("");
+  const [aha, setAha] = useState(1);
+  const [open, setOpen] = useState(0);
+
+  let refs = {};
+
+  for(const key in data) {
+    refs[key] = useRef();
+  }
+
+  const [popup, setPopup] = useState({
+    visible: 0,
+    interval: 0,
+    transaction: {},
+    category: [],
+    product: {},
+    recheck: {},
+ });
+
+  const setLazyPopup = ({visible, transaction, category, product, interval, recheck}) => {
+    setPopup({
+      visible: isNaN(visible) ? popup.visible : visible,
+      transaction: transaction || popup.transaction,
+      category: category || popup.category,
+      product: product || popup.product,
+      recheck: recheck || popup.recheck,
+      interval: interval || popup.interval
+    });
+  }
+
+  const trans = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAACvCAQAAACWCnycAAAAEElEQVR42mNkYGAcRaMIjAC2hQCwhGuphQAAAABJRU5ErkJggg==';
+
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, [])
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>ShopCS</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <menu onClick={_ => setOpen(!open)}>{open ? 'CLOSE' : 'MENU'}</menu>
+
+      <header>
+        <h1 className={styles.title}>ShopCS</h1>
+      </header>
+
+      <nav style={open ? {height: '100vh', position: 'fixed'} : {}}>
+        {Object.keys(refs).map((_, i) => {
+          return <h3 key={i} onClick={e => {scrollTo({top: refs[_].current.offsetTop, behavior: 'smooth'}); open && setOpen(0)}}>{_}</h3>
+        })}
+      </nav>
+
+      <div className={styles.search}>
+        <div className={styles.innersearch}>
+          <input onChange={_ => setQuery(_.target.value.toLowerCase())} placeholder="SEARCH"></input>
+          <i className={styles.sicon} />
+        </div>
+      </div>
+
+
+      <main className={styles.main}>
+        <div className={styles.categories}>
+          {data ? Object.keys(data).reverse().map((_, i) => {
+            const prod = Object.keys(data[_]).sort((a,b)=>a>b)
+              .filter(
+                  (a) => query
+                  ? a.toLowerCase().includes(query)
+                  : 1
+              )
+            return <div key={i}>
+              <h1 ref={refs[_]}>{prod.length ? _ : ''}</h1>
+              <div className={styles.categories}>
+                {prod.map((a, i) => {
+                  data[_][a].img = null;
+                  for(let e = 0; e < data[_][a].length; e++) {
+                    if(data[_][a][e]['img'] !== 'none') {
+                      data[_][a]['img'] = data[_][a][e]['img'];
+                      break;
+                    }
+                  }
+
+                  return (
+                    <div key={a} data-aos="fade-up" className={styles.category} onClick={
+                      () => setPopup({
+                        category: data[_][a],
+                        visible: 1,
+                        transaction: popup.transaction,
+                        product: {},
+                        recheck: popup.recheck
+                      })
+                    }>
+                  
+                    <div className={styles.img} style={{'backgroundImage': `url(${data[_][a]['img']})`}} />
+                      <h4>{a}</h4>
+                    </div>
+                  )
+                })}
+              </div>
+            </div>  
+          }): ''}
+        </div>
+
+
+        {popup.visible ? <PopUp update={setLazyPopup} info={popup} /> : ''}
+      </main>
+    </div>
+  )
+}
+
+Home.getInitialProps = async ctx => {
+  try {
+    return await (await fetch('http://localhost:3000/api/products')).json();
+  } catch {
+    return { error: 'Contact the shop owner as soon as possible' }
+  }
+}
