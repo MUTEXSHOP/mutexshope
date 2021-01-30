33377fc59057ce7219ee0ef23550a3d6f162411c
MUTEXSHOP <77591043+MUTEXSHOP@users.noreply.github.com>
2021-01-31 00:05:04 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -56,7 +56,7 @@ export default function Home({ data }) {
       <menu onClick={_ => setOpen(!open)}>{open ? 'CLOSE' : 'MENU'}</menu>
 
       <header>
-       <div className={styles.logo}><img src="https://i.imgur.com/Ya8tiTA.png"></div>
+       <div class="logo"><img src="https://i.imgur.com/Ya8tiTA.png"></div>
       </header>
 
       <nav style={open ? {height: '100vh', position: 'fixed'} : {}}>
